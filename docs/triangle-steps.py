20097bb2a5632528ad87622c0068f4066b6c34e2
Lars Kellogg-Stedman <lars@redhat.com>
2016-02-01 23:10:15 -0500

    more additive synth examples

--- docs/triangle-steps.py
+++ docs/triangle-steps.py
@@ -7,10 +7,11 @@ mainfig = plt.figure(figsize=(8, 10))
 t = np.linspace(0, np.pi, 500)
 
 for p, order in enumerate(range(0, 10)):
-
     d = [
-        (-1**k) * np.sin(2 * np.pi * k * t)/k
-        for k in range(1, (order+2))
+        (-1)**k *
+        np.sin(2 * np.pi * (2 * k + 1) * t) /
+        (2 * k + 1)**2
+        for k in range(0, (order+1))
     ]
 
     ax = mainfig.add_subplot(10, 2, (2 * p)+1)
@@ -21,7 +22,7 @@ for p, order in enumerate(range(0, 10)):
     ax.set_ylim([-1.5, 1.5])
     ax.plot(d[-1])
 
-    d = (2/np.pi) * sum(d)
+    d = (8/(np.pi**2)) * sum(d)
     ax = mainfig.add_subplot(10, 2, (2*p)+2)
     ax.yaxis.set_ticks([-2, -1, 0, 1, 2])
     ax.xaxis.set_ticks([])
@@ -30,4 +31,4 @@ for p, order in enumerate(range(0, 10)):
     ax.plot(d)
 
 mainfig.tight_layout()
-mainfig.savefig('sawtooth-steps.svg')
+mainfig.savefig('triangle-steps.svg')
