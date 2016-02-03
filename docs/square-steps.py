470984b9cd658344158589c4177b678c6c53f4c9
Lars Kellogg-Stedman <lars@redhat.com>
2016-02-02 22:04:15 -0500

    remove np. prefix for readability

--- docs/square-steps.py
+++ docs/square-steps.py
@@ -1,15 +1,15 @@
 from __future__ import division
 
-import numpy as np
+from numpy import linspace, sin, pi
 import matplotlib.pyplot as plt
 
 mainfig = plt.figure(figsize=(8, 10))
-t = np.linspace(0, np.pi, 500)
+t = linspace(0, pi, 500)
 
 for p, order in enumerate(range(0, 10)):
 
     d = [
-        np.sin(2 * np.pi * (2 * k - 1) * t)/(2 * k - 1)
+        sin(2 * pi * (2 * k - 1) * t)/(2 * k - 1)
         for k in range(1, (order+2))
     ]
 
@@ -21,7 +21,7 @@ for p, order in enumerate(range(0, 10)):
     ax.set_ylim([-1.5, 1.5])
     ax.plot(d[-1])
 
-    d = (4/np.pi) * sum(d)
+    d = (4/pi) * sum(d)
     ax = mainfig.add_subplot(10, 2, (2*p)+2)
     ax.yaxis.set_ticks([-2, -1, 0, 1, 2])
     ax.xaxis.set_ticks([])
--- docs/triangle-steps.py
+++ docs/triangle-steps.py
@@ -1,15 +1,15 @@
 from __future__ import division
 
-import numpy as np
+from numpy import linspace, sin, pi
 import matplotlib.pyplot as plt
 
 mainfig = plt.figure(figsize=(8, 10))
-t = np.linspace(0, np.pi, 500)
+t = linspace(0, pi, 500)
 
 for p, order in enumerate(range(0, 10)):
     d = [
         (-1)**k *
-        np.sin(2 * np.pi * (2 * k + 1) * t) /
+        sin(2 * pi * (2 * k + 1) * t) /
         (2 * k + 1)**2
         for k in range(0, (order+1))
     ]
@@ -22,7 +22,7 @@ for p, order in enumerate(range(0, 10)):
     ax.set_ylim([-1.5, 1.5])
     ax.plot(d[-1])
 
-    d = (8/(np.pi**2)) * sum(d)
+    d = (8/(pi**2)) * sum(d)
     ax = mainfig.add_subplot(10, 2, (2*p)+2)
     ax.yaxis.set_ticks([-2, -1, 0, 1, 2])
     ax.xaxis.set_ticks([])
